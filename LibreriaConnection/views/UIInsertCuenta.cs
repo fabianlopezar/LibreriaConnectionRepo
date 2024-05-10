da769436036d54d824294fb4a8a0baf26fc11eef
FABIAN ESTEBAN LOPEZ ARIAS <fabian_esteban.lopez@uao.edu.co>
2024-05-10 11:14:06 -0500

    [Inicio] Insert Editoriales.

---
+++ LibreriaConnection/views/UIInsertCuenta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibreriaConnection.views
+{
+    public partial class UIInsertCuenta : Form
+    {
+        public UIInsertCuenta()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
